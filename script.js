c36493215d69ea9df19129ce45d865e22aa86133
albyh <albyheredia@gmail.com>
2016-04-08 11:09:33 -0700

    Introduction to jQuery

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
-/*****************************************************************************************/
-/********************************** Rock, Paper, Scissors*********************************/
+
+$( document ).ready(function() {
+  //var a = 4;
+  //console.log(a);
+  $('h1').css('color', 'blue');
+  $('body').css('background-color', '#EFF');
+  $('#everything').css('font-size', '1.5rem');
+  $('#everything').prepend('<button id="buttonFadeOut" class="btn btn-primary">Fade Out</button>');
+  $('#buttonFadeOut').on('click', function() {
+    $('h1').fadeOut('slow');
+  });
+  $('#everything').prepend('<button id="buttonFadeIn" class="btn btn-primary">Fade In</button>');
+  $('#buttonFadeIn').on('click', function() {
+    $('h1').fadeIn(5000);
+  });
+  $('#change-me').css('display', 'inline');
+  $('#secret').css('display', 'inline');
+  $('li').append('<p>New text</p>');
+  
+  var newDiv = $('<h1 class="solution text-center">A new H1 in the header</h1>');
+  $('header').append(newDiv);
+
+  $('header').append('<button id="changebtn" class="btn btn-primary">Change</button>');
+  $('#changebtn').on('click', function() {
+  		newDiv.text("You clicked the button!");
+  	});
+
+  //add a click handler to the link:
+  //For click and most other events, you can prevent the default behavior by calling 
+  //event.preventDefault() in the event handler:
+  /*$( 'a' ).click( function( event) {
+  	alert( 'This prevents default link click behavior');
+  	event.preventDefault();
+  });*/
+
+  //add the .addClass() call to the script:
+  $('a').addClass('test');
+  //To remove an existing class, use .removeClass():
+  $('a').removeClass('test');
+  
+  // the link slowly disappears when clicked.
+  $('a').click( function(event) {
+  	event.preventDefault();
+  	$( this ).hide( 'slow' );
+  });
 
 
-function rockPaperScissor() {
-
-
-	var play = true; 
-	var gameNbr = 1;
-
-//	while (play) {
-
-	document.getElementById('rps').focus();
-
-	var userPick = (document.getElementById( 'rps' ).value)==="4" ? 
-	Math.floor((Math.random() * 3) + 1) : 
-	Number( document.getElementById( 'rps' ).value );
-
-	var computerPick = Math.floor((Math.random() * 3) + 1); 
-	// 1 = Rock | 2 = Paper | 3 = Scissors
-
-	var resultNode = document.createElement('p');
-	var appendToNode = document.getElementById( 'mySection' );
-
-	if ( userPick === 1 && computerPick === 2) {
-		resultNode.innerHTML = "SORRY! Paper covers Rock";
-	} else if ( userPick === 1 && computerPick === 3) {
-		resultNode.innerHTML = "YOU WIN! Rock beats Scissors!";
-	} else if ( userPick === 2 && computerPick === 1 ){
-		resultNode.innerHTML = "YOU WIN! Paper covers Rock!";
-	} else if ( userPick === 2 && computerPick === 3 ){
-		resultNode.innerHTML = "SORRY! Scissors cut Paper";
-	} else if ( userPick === 3 && computerPick === 1 ){
-		resultNode.innerHTML = "SORRY! Rock beats Scissors (literally!)";
-	} else if ( userPick === 3 && computerPick === 2 ){
-		resultNode.innerHTML = "YOU WIN! Scissors cuts paper!";
-	} else {
-		resultNode.innerHTML = "Tie";
-	}
-
-	resultNode.innerHTML = "Game " + gameNbr + " - " +resultNode.innerHTML;
-	appendToNode.appendChild( resultNode );
-	gameNbr++;
-
-	play = confirm( resultNode.innerHTML + "\nSo, do you want to play again?");
-
-//	}   // while (play)
-
-}
+  });
