7e7dba150b6c2656430de7360478f8c5b4754cca
albyh <albyheredia@gmail.com>
2016-06-14 22:19:10 -0700

    Implementing dropdown menu

---
+++ webhut.js
@@ -0,0 +1,24 @@
+﻿$(document).ready( function() {
+    
+    $('#dropdown').on('mouseenter', function() {
+        $('.submenu').show();
+    })
+
+    $('#dropdown li .subitem').on('mouseleave', function () {
+        $('.submenu').hide();
+    })
+    //$('.submenu').on('mouseleave').hide('.submenu')
+
+
+    /*
+    var menuItem = ['Item 1', 'Item 2', 'Item 3'];
+   
+    $.each( menuItem , function( i , val ){
+        $('#dropdown').on('mouseenter', function ()
+        {
+            $('<li />', {  text: val }).appendTo('#dropdown')
+        });
+    });
+    */
+
+})
