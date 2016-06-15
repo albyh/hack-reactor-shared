f7b2eb01323ee0efa0faf0f79a0ecdee03edc7a0
albyh <albyheredia@gmail.com>
2016-06-15 10:43:47 -0700

    Fixed mouseenter/mouseleave for dropdown.

--- webhut.js
+++ webhut.js
@@ -1,24 +1,10 @@
 ﻿$(document).ready( function() {
     
-    $('#dropdown').on('mouseenter', function() {
+    $('#dropdown').on('mouseenter', function () {
         $('.submenu').show();
     })
 
-    $('#dropdown li .subitem').on('mouseleave', function () {
+    $("#dropdown").on('mouseleave', function () {
         $('.submenu').hide();
     })
-    //$('.submenu').on('mouseleave').hide('.submenu')
-
-
-    /*
-    var menuItem = ['Item 1', 'Item 2', 'Item 3'];
-   
-    $.each( menuItem , function( i , val ){
-        $('#dropdown').on('mouseenter', function ()
-        {
-            $('<li />', {  text: val }).appendTo('#dropdown')
-        });
-    });
-    */
-
 })
